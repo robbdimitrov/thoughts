f05002ec32964b78ff7ff5948789dbf45d858dca
Robert Dimitrov <robert_dimitrov@me.com>
2018-12-31 20:09:21 +0200

    Added context element to ThoughtItem

--- src/frontend/src/components/ThoughtItem.vue
+++ src/frontend/src/components/ThoughtItem.vue
@@ -1,34 +1,55 @@
 <template>
   <article class="container">
-    <img class="avatar"
-      src="https://bulma.io/images/placeholders/96x96.png" />
-    <div class="content">
-      <p class="profile-action">
-        <strong class="name">John Doe</strong>
-        <small class="username">@johndoe</small>
-        <small class="time">3h</small>
-      </p>
+    <div class="thought-context">
+      <i class="context-icon fas fa-retweet"></i>
+      <span class="context-label">Jonathan Key retweeted</span>
+    </div>
 
-      <p class="text">Hello beautiful world! My first post!</p>
+    <div class="thought-content">
+      <img class="avatar"
+        src="https://bulma.io/images/placeholders/96x96.png" />
+      <div class="content">
+        <p class="profile-action">
+          <strong class="name">John Doe</strong>
+          <small class="username">@johndoe</small>
+          <small class="time">3h</small>
+        </p>
 
-      <div class="actions">
-        <button class="retweet-button">
-          <i class="button-icon fas fa-retweet"></i>
-          <span class="button-label">2</span>
-        </button>
+        <p class="text">Hello beautiful world! My first post!</p>
 
-        <button class="like-button">
-          <i class="button-icon fas fa-heart"></i>
-          <span class="button-label">3</span>
-        </button>
+        <div class="actions">
+          <button class="retweet-button">
+            <i class="button-icon fas fa-retweet"></i>
+            <span class="button-label">2</span>
+          </button>
+
+          <button class="like-button">
+            <i class="button-icon fas fa-heart"></i>
+            <span class="button-label">3</span>
+          </button>
+        </div>
       </div>
     </div>
+
   </article>
 </template>
 
 <style lang="scss" scoped>
-.container, .actions, button {
+.thought-content, .actions, button {
   display: flex;
+}
+
+.context-icon {
+  margin-left: (48px + 10px) - 16px;
+  width: 16px;
+  margin-right: 10px;
+}
+
+.thought-context {
+  margin-top: 10px;
+  color: gray;
+  font-size: 0.8em;
+  font-weight: 400;
 }
 
 .profile-action {
