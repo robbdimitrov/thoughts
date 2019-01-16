78a03bb5ccd76946456b816ffd119305de591bc9
Robert Dimitrov <robert_dimitrov@me.com>
2019-01-16 23:42:40 +0200

    Initial styling for search view.

--- src/frontend/src/views/SearchView.vue
+++ src/frontend/src/views/SearchView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
     <div class="search-term-bar">
-      <span class="search-term">Search term</span>
+      <div class="search-term-container">
+        <span class="search-term">Search term</span>
+      </div>
     </div>
 
     <div class="scope-nav-bar">
@@ -20,14 +22,14 @@
       </div>
     </div>
 
-    <div class="content">
+    <div class="content main-content">
       <div class="user-container">
         <div class="user-header">
           <span>People</span>
           <span>View all</span>
         </div>
 
-        <user-list :items="2" class="user-container" />
+        <user-list :items="2" />
       </div>
 
       <thought-list class="thought-container" :items="3" />
@@ -49,5 +51,39 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../styles/variables";
+@import "../styles/fonts";
 
+$search-term-background: #35393C;
+
+.content {
+  margin-bottom: $spacing-md;
+}
+
+.search-term-bar {
+  background: $search-term-background;
+  height: 32px;
+  padding: 16px 0;
+}
+
+.search-term-container {
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: $max-component-width;
+}
+
+.search-term {
+  padding-left: 12px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  font-size: 27px;
+  line-height: 32px;
+  color: white;
+  font-weight: $font-weight-semibold;
+}
+
+.user-container {
+  margin-bottom: $padding;
+}
 </style>
