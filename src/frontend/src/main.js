68361858555de66a6b4d5a1788e7237e1e6ee621
Robert Dimitrov <robert_dimitrov@me.com>
2019-01-21 08:35:32 +0200

    Added initial state file

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
-import { createRouter } from './router/index';
+import { router } from './router/index';
+import { store } from './store/index';
 
 Vue.config.productionTip = false;
 
-const router = createRouter();
-
 new Vue({
+    store,
     router,
     render: h => h(App),
 }).$mount('#app');
--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -13,24 +13,22 @@ const SignupView = () => import('../views/SignupView.vue');
 const ThoughtView = () => import('../views/ThoughtView.vue');
 const UserView = () => import('../views/UserView.vue');
 
-export function createRouter () {
-    return new VueRouter({
-        mode: 'history',
-        routes: [
-            { path: '/feed', component: FeedView },
-            { path: '/login', component: LoginView },
-            { path: '/signup', component: SignupView },
-            { path: '/search', component: SearchView },
-            { path: '/thought', component: ThoughtView },
-            { path: '/user/:location*', component: UserView },
-            { path: '/settings', component: SettingsView,
-                children: [
-                    { path: 'account', component: ProfileView },
-                    { path: 'password', component: PasswordView },
-                    { path: '', redirect: 'account' }
-                ]
-            },
-            { path: '/', redirect: '/feed' }
-        ]
-    });
-}
+export const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        { path: '/feed', component: FeedView },
+        { path: '/login', component: LoginView },
+        { path: '/signup', component: SignupView },
+        { path: '/search', component: SearchView },
+        { path: '/thought', component: ThoughtView },
+        { path: '/user/:location*', component: UserView },
+        { path: '/settings', component: SettingsView,
+            children: [
+                { path: 'account', component: ProfileView },
+                { path: 'password', component: PasswordView },
+                { path: '', redirect: 'account' }
+            ]
+        },
+        { path: '/', redirect: '/feed' }
+    ]
+});
