a7cb77b6fb782430ee1fd73d6e3daf2c14749649
Robert Dimitrov <robert_dimitrov@me.com>
2019-04-09 18:00:16 +0300

    Implemented logic for regenerating tokens

--- src/authservice/authservice/session.py
+++ src/authservice/authservice/session.py
@@ -1,10 +1,11 @@
 from flask import (
     Blueprint, request, make_response, jsonify
 )
+import datetime
 import jwt
+import os
 
-from authservice import db_client, status
-from authservice.db_client import DBException
+from authservice import db_client
 
 
 bp = Blueprint('session', __name__)
@@ -12,14 +13,54 @@ bp = Blueprint('session', __name__)
 
 @bp.route('/session', methods=['POST'])
 def create_session(username):
-    """Follows or unfollows a user with the current user."""
+    """Create a new access and refresh tokens."""
 
-    # encoded = jwt.encode({'some': 'payload'}, 'secret', algorithm='HS256')
-    # jwt.decode(encoded, 'secret', algorithms=['HS256'])
-    return make_response(jsonify({'response': 'Created session.'}), status.OK)
+    content = request.get_json()
+
+    refresh_token = content.get('refreshToken')
+    secret = os.getenv('JWT_SECRET')
+
+    if refresh_token is not None:
+        try:
+            payload = jwt.decode(refresh_token, secret, algorithms='HS256')
+        except jwt.ExpiredSignatureError:
+            error = {'code': 401, 'error': 'expired_token', 'error_description': 'Refresh token is expired.'}
+            return make_response(jsonify({'error': error}), 401)
+
+        session_id = payload['sessionId']
+
+        session = db_client.get_session(session_id)
+
+        if session is None:
+            error = {'code': 401, 'error': 'invalid_session', 'error_description': 'Invalid session.'}
+            return make_response({'error': error}, 401)
+
+        access_payload = {'userId': session['user_id'],
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=15)}
+
+        refresh_payload = {'sessionId': session['session_id'],
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=7)}
+
+        access_token = jwt.encode(access_payload, secret, algorithm='HS256')
+        refresh_token = jwt.encode(refresh_payload, secret, algorithm='HS256')
+
+        make_response(jsonify({'token_type': 'bearer',
+            'access_token': access_token,
+            'refresh_token': refresh_token}), 200)
+
+    username = content.get('username')
+    email = content.get('email')
+
+    # TODO: validate email, create a new session in the db, create and return tokens
+    # TODO: return invalid email/ invalid request when all else fails
+    # TODO: create DB methods
+    # TODO: simplify the code, extract to additional methods
+    # TODO: create RPC call for the validity of the token
+
+    return make_response(jsonify({'response': 'Created session.'}), 200)
 
 
 @bp.route('/session', methods=['DELETE'])
 def delete_session(username):
     """Delete a session with a given token."""
-    return make_response(jsonify({'response': 'Deleted session.'}), status.OK)
+    return make_response(jsonify({'response': 'Deleted session.'}), 200)
