33437ed383ca97a40d6fded70137a492ef69a40a
Robert Dimitrov <robert_dimitrov@me.com>
2019-04-22 09:28:23 +0300

    - Created api gw project - Added pb auth_service file

--- src/authservice/authservice/scheme.sql
+++ src/authservice/authservice/scheme.sql
@@ -3,6 +3,7 @@ CREATE SCHEMA IF NOT EXISTS thoughts;
 CREATE TABLE IF NOT EXISTS thoughts.sessions (
   id serial PRIMARY KEY,
   name varchar(80) NOT NULL,
+  user_agent varchar(100) NOT NULL,
   user_id integer REFERENCES thoughts.users(id) ON DELETE CASCADE,
   date_created timestamp NOT NULL DEFAULT localtimestamp
 );
