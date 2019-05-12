6cfc080146f66505cf02ccce554f1738bf2cd8d7
Robert Dimitrov <robert_dimitrov@me.com>
2019-05-12 21:41:39 +0300

    Implemented AuthClient

--- src/apiservice/src/clients/auth-client.js
+++ src/apiservice/src/clients/auth-client.js
@@ -7,9 +7,67 @@ export class AuthClient {
 
     this.authClient = new services.AuthServiceClient(this.authURI,
       grpc.credentials.createInsecure());
-      this.sessionClient = new services.SessionServiceClient(this.authURI,
+    this.sessionClient = new services.SessionServiceClient(this.authURI,
       grpc.credentials.createInsecure());
   }
+
+  // Authentication
+
+  createSession(email, password, userAgent) {
+    let request = new messages.Credentials();
+    request.setEmail(email);
+    request.setPassword(password);
+    request.setUserAgent(userAgent);
+
+    return new Promise((res, rej) => {
+      this.authClient.login(request, (err, response) => {
+        if (err) {
+          return rej(err);
+        }
+        let error = response.getError();
+        if (error !== undefined) {
+          return res.status(error.getCode()).send({
+            'code': error.getCode(),
+            'error': error.getError(),
+            'message': error.getMessage()
+          });
+        }
+        res.send({
+          'token_type': respose.getTokenType(),
+          'access_token': response.getAccessToken(),
+          'refresh_token': response.getRefreshToken()
+        });
+      });
+    });
+  }
+
+  refreshToken(token) {
+    let request = new messages.AuthRequest();
+    request.setToken(token);
+
+    return new Promise((res, rej) => {
+      this.authClient.refresh(request, (err, response) => {
+        if (err) {
+          return rej(err);
+        }
+        let error = response.getError();
+        if (error !== undefined) {
+          return res.status(error.getCode()).send({
+            'code': error.getCode(),
+            'error': error.getError(),
+            'message': error.getMessage()
+          });
+        }
+        res.send({
+          'token_type': respose.getTokenType(),
+          'access_token': response.getAccessToken(),
+          'refresh_token': response.getRefreshToken()
+        });
+      });
+    });
+  }
+
+  // Sessions
 
   getSessions(token) {
     let request = new messages.AuthRequest();
@@ -22,6 +80,31 @@ export class AuthClient {
         }
         let sessions = response.getSessions();
         res(JSON.stringify(sessions));
+      });
+    });
+  }
+
+  deleteSession(sessionId, token) {
+    let request = new messages.AuthRequest()
+    request.setSessionId(sessionId);
+    request.setToken(token);
+
+    return new Promise((res, rej) => {
+      this.sessionClient.deleteSession(request, (err, response) => {
+        if (err) {
+          return rej(err);
+        }
+        let error = response.getError();
+        if (error !== undefined) {
+          return res.status(error.getCode()).send({
+            'code': error.getCode(),
+            'error': error.getError(),
+            'message': error.getMessage()
+          });
+        }
+        res.send({
+          'message': respose.getMessage()
+        });
       });
     });
   }
