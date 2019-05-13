ca8eac92ced2223ff299feda6fd2ff58928e2e24
Robert Dimitrov <robert_dimitrov@me.com>
2019-05-13 20:39:39 +0300

    Implemented PostRouter

--- src/apiservice/src/routers/post-router.js
+++ src/apiservice/src/routers/post-router.js
@@ -46,40 +46,136 @@ export class PostRouter {
   // Posts
 
   createPost(req, res) {
+    let content = req.body.content;
+    let token = getToken(req);
 
+    this.postClient.createPost(content, token)
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        let code = err['code'] !== undefined ? err.code : 400;
+        res.status(code).send(err);
+      });
   }
 
   getPost(req, res) {
+    let postId = req.params.id;
+    let token = getToken(req);
 
+    this.postClient.getPost(postId, token)
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        let code = err['code'] !== undefined ? err.code : 400;
+        res.status(code).send(err);
+      });
   }
 
   getPosts(req, res) {
+    let username = req.params.username;
+    let token = getToken(req);
+    let page = parseInt(req.query.page) || 0;
+    let limit = parseInt(req.query.limit) || 20;
+    let countOnly = (parseInt(req.query.count) || 0) === 1;
 
+    this.postClient.getPosts(username, token, page, limit, countOnly)
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        let code = err['code'] !== undefined ? err.code : 400;
+        res.status(code).send(err);
+      });
   }
 
   getLikedPosts(req, res) {
+    let username = req.params.username;
+    let token = getToken(req);
+    let page = parseInt(req.query.page) || 0;
+    let limit = parseInt(req.query.limit) || 20;
+    let countOnly = (parseInt(req.query.count) || 0) === 1;
 
+    this.postClient.getLikedPosts(username, token, page, limit, countOnly)
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        let code = err['code'] !== undefined ? err.code : 400;
+        res.status(code).send(err);
+      });
   }
 
   deletePost(req, res) {
+    let postId = req.params.id;
+    let token = getToken(req);
 
+    this.postClient.deletePost(postId, token)
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        let code = err['code'] !== undefined ? err.code : 400;
+        res.status(code).send(err);
+      });
   }
 
   // Actions
 
   likePost(req, res) {
+    let postId = req.params.id;
+    let token = getToken(req);
 
+    this.postClient.likePost(postId, token)
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        let code = err['code'] !== undefined ? err.code : 400;
+        res.status(code).send(err);
+      });
   }
 
   unlikePost(req, res) {
+    let postId = req.params.id;
+    let token = getToken(req);
 
+    this.postClient.unlikePost(postId, token)
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        let code = err['code'] !== undefined ? err.code : 400;
+        res.status(code).send(err);
+      });
   }
 
   retweetPost(req, res) {
+    let postId = req.params.id;
+    let token = getToken(req);
 
+    this.postClient.retweetPost(postId, token)
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        let code = err['code'] !== undefined ? err.code : 400;
+        res.status(code).send(err);
+      });
   }
 
   removeRetweet(req, res) {
+    let postId = req.params.id;
+    let token = getToken(req);
 
+    this.postClient.removeRetweet(postId, token)
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        let code = err['code'] !== undefined ? err.code : 400;
+        res.status(code).send(err);
+      });
   }
 }
