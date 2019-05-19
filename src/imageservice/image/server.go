c2921c1e3538d3efab9446ea51c9aec7959b9bc6
Robert Dimitrov <robert_dimitrov@me.com>
2019-05-19 23:50:26 +0300

    WIP: Image upload

--- src/imageservice/image/server.go
+++ src/imageservice/image/server.go
@@ -1,25 +1,89 @@
-package post
+package image
+
+import (
+	"crypto/rand"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+)
 
 // Server is runing on a port and handling grpc requests
 type Server struct {
-	port string
+	port      string
+	imagePath string
 }
 
 // NewServer is a constructor for new Server objects
-func NewServer(port string) *Server {
-	return &Server{port}
+func NewServer(port string, imagePath string) *Server {
+	return &Server{port, imagePath}
+}
+
+func (s *Server) uploadFile(w http.ResponseWriter, r *http.Request) {
+	log.Println("File Upload Endpoint Hit")
+
+	r.ParseMultipartForm(10 << 20)
+
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		log.Println("Error Retrieving the File")
+		log.Println(err)
+		return
+	}
+	defer file.Close()
+
+	s.saveFile(w, file)
+}
+
+func (s *Server) saveFile(w http.ResponseWriter, file multipart.File) {
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+
+	mimeType := http.DetectContentType(data)
+	switch mimeType {
+	case "image/jpeg", "image/jpg", "image/png":
+		break
+	default:
+		jsonResponse(w, http.StatusBadRequest, "The format file is not valid.")
+		return
+	}
+
+	path := fmt.Sprintf("%s/", s.imagePath)
+	err = ioutil.WriteFile(path+randToken(12), data, 0666)
+	if err != nil {
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+	jsonResponse(w, http.StatusCreated, "File uploaded successfully!.")
+}
+
+func jsonResponse(w http.ResponseWriter, code int, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	fmt.Fprint(w, message)
+}
+
+func randToken(len int) string {
+	b := make([]byte, len)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
+}
+
+func (s *Server) setupRoutes() {
+	fs := http.FileServer(http.Dir(s.imagePath))
+	http.Handle("/images/", http.StripPrefix("/images", fs))
+
+	http.HandleFunc("/upload", s.uploadFile)
 }
 
 // Start starts the server instance
 func (s *Server) Start() {
-	// log.Printf("Starting server on port %s\n", s.port)
+	s.setupRoutes()
+	log.Printf("Starting server on port %s\n", s.port)
 
-	// lis, err := net.Listen("tcp", ":"+s.port)
-	// if err != nil {
-	// 	log.Fatalf("Server failed to listen: %v", err)
-	// }
-
-	// if err := svr.Serve(lis); err != nil {
-	// 	log.Fatalf("Server failed to serve: %v", err)
-	// }
+	http.ListenAndServe(fmt.Sprintf(":%s", s.port), nil)
 }
--- src/imageservice/main.go
+++ src/imageservice/main.go
@@ -3,14 +3,10 @@ package main
 import (
 	"os"
 
-	_ "github.com/lib/pq"
-
-	"postservice/post"
+	"imageservice/image"
 )
 
 func main() {
-	server := post.NewServer(os.Getenv("PORT"),
-		os.Getenv("DB_URI"), os.Getenv("AUTH_URI"),
-		os.Getenv("USER_URI"))
+	server := image.NewServer(os.Getenv("PORT"))
 	server.Start()
 }
