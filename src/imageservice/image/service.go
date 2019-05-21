87e8dbd7759f7ab4a26d3e43ce943c3967d8db75
Robert Dimitrov <robert_dimitrov@me.com>
2019-05-21 21:14:10 +0300

    Moved upload logic to service

---
+++ src/imageservice/image/service.go
@@ -0,0 +1,83 @@
+package image
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+)
+
+const maxFileSize = 1 << 20
+
+// Service handles validating and saving of files
+type Service struct {
+	imagePath string
+}
+
+// NewService creates a new image upload service instance
+func NewService(imagePath string) *Service {
+	return &Service{imagePath}
+}
+
+func (s *Service) uploadFile(w http.ResponseWriter, r *http.Request) {
+	log.Println("File Upload Endpoint Hit")
+
+	r.Body = http.MaxBytesReader(w, r.Body, maxFileSize)
+	err := r.ParseMultipartForm(maxFileSize)
+	if err != nil {
+		log.Printf("Error Parsing File %v", err)
+
+		error := Error{http.StatusBadRequest, "FILE_TOO_BIG",
+			"File should be smaller than 1MB."}
+		ErrorResponse(w, error)
+		return
+	}
+
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		log.Printf("Error Retrieving File %v", err)
+
+		error := Error{http.StatusBadRequest, "FILE_ERROR",
+			"There was an error while processing the image."}
+		ErrorResponse(w, error)
+		return
+	}
+	defer file.Close()
+
+	s.saveFile(w, file)
+}
+
+func (s *Service) saveFile(w http.ResponseWriter, file multipart.File) {
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Printf("Error reading file: %v", err)
+		error := Error{http.StatusBadRequest, "FILE_ERROR",
+			"There was an error while processing the image."}
+		ErrorResponse(w, error)
+		return
+	}
+
+	mimeType := http.DetectContentType(data)
+	switch mimeType {
+	case "image/jpeg", "image/jpg", "image/png":
+		break
+	default:
+		error := Error{http.StatusBadRequest, "INVALID_FILE",
+			"The format file is not valid."}
+		ErrorResponse(w, error)
+		return
+	}
+
+	path := fmt.Sprintf("%s/", s.imagePath)
+	filename := RandToken(12)
+	err = ioutil.WriteFile(path+filename, data, 0666)
+	if err != nil {
+		log.Printf("Error writing file %v", err)
+		error := Error{http.StatusBadRequest, "FILE_ERROR",
+			"There was an error while processing the image."}
+		ErrorResponse(w, error)
+		return
+	}
+	SuccessResponse(w, filename)
+}
