9cf1709e6db8602793db10bbdfd62ae15102eae4
Robert Dimitrov <robert_dimitrov@me.com>
2019-05-28 23:17:48 +0300

    - Added env variables to apiservice teplate - Restored port from env variable in apiservice

--- src/apiservice/src/index.js
+++ src/apiservice/src/index.js
@@ -1,10 +1,10 @@
 import { Server } from './server';
 
-const server = new Server(3000);
-server.config['AUTH_URI'] = process.env.AUTH_URI;
-server.config['POST_URI'] = process.env.POST_URI;
-server.config['USER_URI'] = process.env.USER_URI;
-server.config['IMAGE_URI'] = process.env.IMAGE_URI;
+const server = new Server(process.env.PORT);
+server.config['AUTH_URI'] = process.env.AUTH_SERVICE_ADDR;
+server.config['POST_URI'] = process.env.POST_SERVICE_ADDR;
+server.config['USER_URI'] = process.env.USER_SERVICE_ADDR;
+server.config['IMAGE_URI'] = process.env.IMAGE_SERVICE_ADDR;
 
 if (!module.parent) {
   server.start();
