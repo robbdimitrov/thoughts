68ae3ae92352634e8256d44e12060a99cb08df1e
Robert Dimitrov <robert_dimitrov@me.com>
2019-06-15 20:55:28 +0300

    Implemented liked posts query

--- src/postservice/post/db_client.go
+++ src/postservice/post/db_client.go
@@ -2,6 +2,7 @@ package post
 
 import (
 	"errors"
+	"fmt"
 
 	pb "postservice/genproto"
 )
@@ -16,24 +17,43 @@ func NewDbClient(db *Database) *DbClient {
 	return &DbClient{db}
 }
 
+func (c *DbClient) createPostQuery(where string, order string, limit string) string {
+	query := fmt.Sprintf(`SELECT posts.id, posts.content, posts.user_id,
+		count(distinct likes.id) AS likes,
+		count(distinct retweets.id) AS retweets,
+		time_format(posts.date_created) AS date_created
+		FROM  thoughts.posts AS posts  LEFT JOIN thoughts.likes AS likes
+		ON likes.post_id = posts.id LEFT JOIN thoughts.retweets AS retweets
+		ON retweets.post_id = posts.id
+		%s
+		GROUP BY posts.id
+		%s %s`, where, order, limit)
+	return query
+}
+
 // CreatePost creates a new Post row in the database
 func (c *DbClient) CreatePost(content string, userID int32) (pb.Post, error) {
 	conn := c.db.GetConn()
 
-	row := conn.QueryRow(`INSERT INTO thoughts.posts(content, user_id) VALUES($1, $2)
-    RETURNING id, content, user_id, time_format(date_created)`,
+	row := conn.QueryRow(`INSERT INTO thoughts.posts(content, user_id)
+		VALUES($1, $2) RETURNING id`,
 		content, userID)
 
-	post, err := rowToPost(row)
-	return post, err
+	var id int32
+
+	err := row.Scan(&id)
+	if err != nil {
+		return pb.Post{}, errors.New("Error happened while retrieving post")
+	}
+
+	return c.GetPost(id)
 }
 
 // GetPost returns a Post row with the passed id from the database
 func (c *DbClient) GetPost(id int32) (pb.Post, error) {
 	conn := c.db.GetConn()
 
-	row := conn.QueryRow(`SELECT id, content, user_id, time_format(date_created)
-    FROM thoughts.posts WHERE id = $1`, id)
+	row := conn.QueryRow(c.createPostQuery("WHERE id = $1", "", ""), id)
 
 	post, err := rowToPost(row)
 	return post, err
@@ -90,11 +110,11 @@ func (c *DbClient) GetPosts(userID int32, page int32, limit int32) (pb.Posts, er
 func (c *DbClient) GetLikedPosts(userID int32, page int32, limit int32) (pb.Posts, error) {
 	conn := c.db.GetConn()
 
-	rows, err := conn.Query(`SELECT id, content, user_id, time_format(date_created)
-    FROM thoughts.posts
-    WHERE id IN (SELECT post_id FROM thoughts.likes WHERE user_id = $1
-    ORDER BY date_created DESC) OFFSET $2 LIMIT $3`,
-		userID, page*limit, limit)
+	query := c.createPostQuery(
+		"WHERE posts.id IN (SELECT post_id FROM thoughts.likes WHERE user_id = $1",
+		"ORDER BY posts.date_created DESC)",
+		"OFFSET $2 LIMIT $3")
+	rows, err := conn.Query(query, userID, page*limit, limit)
 	if err != nil {
 		return pb.Posts{}, errors.New("Error happened while reading from the database")
 	}
@@ -108,7 +128,7 @@ func (c *DbClient) GetLikedPosts(userID int32, page int32, limit int32) (pb.Post
 func (c *DbClient) DeletePost(postID int32) error {
 	conn := c.db.GetConn()
 
-	_, err := conn.Exec(`DELETE FROM thoughts.posts WHERE id = $1`, postID)
+	_, err := conn.Exec("DELETE FROM thoughts.posts WHERE id = $1", postID)
 	if err != nil {
 		return errors.New("Error happened while writing to the database")
 	}
