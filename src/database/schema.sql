6d124c1a43bf0c44a99f3b07a38fe456e20301a4
Robert Dimitrov <robert_dimitrov@me.com>
2019-11-04 20:38:25 +0200

    - Renamed date_created property to created_at - Fixed timestamp format

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -10,13 +10,13 @@ CREATE TABLE IF NOT EXISTS users (
   password varchar(255) NOT NULL,
   bio varchar(255) DEFAULT '',
   avatar varchar(255) DEFAULT '',
-  date_created timestamp NOT NULL DEFAULT localtimestamp
+  created_at timestamp NOT NULL DEFAULT now()
 );
 
 CREATE TABLE IF NOT EXISTS followings (
   user_id integer REFERENCES users ON DELETE CASCADE,
   follower_id integer REFERENCES users ON DELETE CASCADE,
-  date_created timestamp NOT NULL DEFAULT localtimestamp,
+  created_at timestamp NOT NULL DEFAULT now(),
   UNIQUE(user_id, follower_id)
 );
 
@@ -26,20 +26,20 @@ CREATE TABLE IF NOT EXISTS posts (
   id serial PRIMARY KEY,
   content varchar(255) NOT NULL,
   user_id integer REFERENCES users ON DELETE CASCADE,
-  date_created timestamp NOT NULL DEFAULT localtimestamp
+  created_at timestamp NOT NULL DEFAULT now()
 );
 
 CREATE TABLE IF NOT EXISTS likes (
   post_id integer REFERENCES posts ON DELETE CASCADE,
   user_id integer REFERENCES users ON DELETE CASCADE,
-  date_created timestamp NOT NULL DEFAULT localtimestamp,
+  created_at timestamp NOT NULL DEFAULT now(),
   UNIQUE(post_id, user_id)
 );
 
 CREATE TABLE IF NOT EXISTS retweets (
   post_id integer REFERENCES posts ON DELETE CASCADE,
   user_id integer REFERENCES users ON DELETE CASCADE,
-  date_created timestamp NOT NULL DEFAULT localtimestamp,
+  created_at timestamp NOT NULL DEFAULT now(),
   UNIQUE(post_id, user_id)
 );
 
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS sessions (
   id serial PRIMARY KEY,
   user_id integer REFERENCES users ON DELETE CASCADE,
   user_agent varchar(255) NOT NULL,
-  date_created timestamp NOT NULL DEFAULT localtimestamp
+  created_at timestamp NOT NULL DEFAULT now()
 );
 
 -- Utils
@@ -57,6 +57,6 @@ CREATE TABLE IF NOT EXISTS sessions (
 CREATE OR REPLACE FUNCTION time_format(origin timestamp)
 RETURNS text AS $$
 BEGIN
-  RETURN to_char(origin, 'DD-MM-YYYY"T"HH24:MI:SS');
+  RETURN to_char(origin, 'YYYY-MM-DD"T"HH24:MI:SS"Z"');
 END;
 $$  LANGUAGE plpgsql;
