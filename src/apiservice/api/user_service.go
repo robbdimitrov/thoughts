4188d099010d121cfd9e891c1a49323149782b56
Robert Dimitrov <robert_dimitrov@me.com>
2020-05-03 21:18:29 +0300

    Connect update user API call

--- src/apiservice/api/user_service.go
+++ src/apiservice/api/user_service.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"context"
+	"strconv"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -53,7 +54,29 @@ func (s *userService) updateUser(c echo.Context) error {
 }
 
 func (s *userService) getUser(c echo.Context) error {
-	return c.JSON(200, user{})
+	conn, err := grpc.Dial(s.addr, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		return echo.NewHTTPError(500, err.Error())
+	}
+	defer conn.Close()
+	client := pb.NewUserServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	userID, err := strconv.Atoi(c.Param("userId"))
+	if err != nil {
+		return echo.NewHTTPError(500, err.Error())
+	}
+	req := pb.UserRequest{UserId: int32(userID)}
+
+	res, err := client.GetUser(ctx, &req)
+	if err != nil {
+		s := status.Convert(err)
+		return echo.NewHTTPError(getStatusCode(s), s.Proto().GetMessage())
+	}
+
+	return c.JSON(200, mapUser(res))
 }
 
 func (s *userService) getFollowing(c echo.Context) error {
