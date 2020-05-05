e1d17e1849983bd533c332876f9fa68c91b63a85
Robert Dimitrov <robert_dimitrov@me.com>
2020-05-05 23:43:25 +0300

    Connect getting images

--- src/apiservice/api/image_service.go
+++ src/apiservice/api/image_service.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -25,25 +24,38 @@ func newImageService(addr string) *imageService {
 func (s *imageService) createImage(c echo.Context) error {
 	url := fmt.Sprintf("http://%s/uploads", s.addr)
 	req, err := http.NewRequest("POST", url, c.Request().Body)
-	req.Header = c.Request().Header
 	if err != nil {
-		log.Printf("Creating request failed %s\n", err)
+		log.Printf("Creating request failed %v", err)
+		return echo.NewHTTPError(500)
+	}
+
+	copyHeader(c.Request(), req, "content-type")
+	copyHeader(c.Request(), req, "content-length")
+
+	res, err := s.client.Do(req)
+	if err != nil {
+		log.Printf("Uploading image failed: %v", err)
+		return echo.NewHTTPError(500)
+	}
+	defer res.Body.Close()
+
+	return c.Stream(res.StatusCode, res.Header.Get("content-type"), res.Body)
+}
+
+func (s *imageService) getImage(c echo.Context) error {
+	url := fmt.Sprintf("http://%s/uploads/%s", s.addr, c.Param("filename"))
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Printf("Creating request failed: %v", err)
 		return echo.NewHTTPError(500)
 	}
 
 	res, err := s.client.Do(req)
 	if err != nil {
-		log.Printf("Uploading image failed %s\n", err)
+		log.Printf("Getting image failed: %v", err)
 		return echo.NewHTTPError(500)
 	}
+	defer res.Body.Close()
 
-	return c.Stream(res.StatusCode, res.Header.Get("Content-Type"), res.Body)
-}
-
-func (s *imageService) getImage(c echo.Context) error {
-	// TODO: Proxy request and response to image service
-
-	fmt.Fprintf(os.Stdout, "[%s] %s", time.Now().Format(time.RFC3339), "message")
-	// return c.Stream(res.StatusCode, res.Header.Get("Content-Type"), res.Body)
-	return c.JSON(200, "CreateImage")
+	return c.Stream(res.StatusCode, res.Header.Get("content-type"), res.Body)
 }
