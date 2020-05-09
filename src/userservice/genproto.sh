2ff93c76590bf1b773f694ba3795904ab68de5cc
Robert Dimitrov <robert_dimitrov@me.com>
2020-05-09 22:42:09 +0300

    Update pb generating scripts, update python dependencies

--- src/userservice/genproto.sh
+++ src/userservice/genproto.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
-PROTODIR=../../pb
-SRC_DIR=./userservice
-
-python -m grpc_tools.protoc -I $PROTODIR --python_out=$SRC_DIR \
---grpc_python_out=$SRC_DIR ../../pb/thoughts.proto
+python -m grpc_tools.protoc -I../../pb --python_out=./userservice \
+--grpc_python_out=./userservice ../../pb/thoughts.proto
